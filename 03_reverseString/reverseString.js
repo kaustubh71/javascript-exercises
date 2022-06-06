7713a10bed43400ec578753482c7e854267faf9f
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-06 14:50:44 -0500

    my solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const contents = string
+    let reverseContent = ""
 
+    if (string !="") {
+        for (let i=contents.length-1; i>=0; i--) {
+            reverseContent += contents[i]
+        }
+        return reverseContent
+    } else {return reverseContent}
 };
+
+// Official solution
+// const reverseString = function(string) {
+//     return string.split('').reverse().join('');
+//    };
+   
 
 // Do not edit below this line
 module.exports = reverseString;
