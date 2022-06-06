e18dcce506d92817ac9591b9a71e2901f6d6b9ac
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-06 14:47:04 -0500

    my solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let greeting = []
+    if (num<0){
+        greeting.push("ERROR")
+    } else if (num>0) {
+        for (let i=0; i<num; i++) {
+        greeting.push(string)
+        }
+    } else if (string === "") {
+        return
+    }
+    return greeting.join("")
+}
 
 // Do not edit below this line
 module.exports = repeatString;
