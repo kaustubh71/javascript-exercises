754853bb2a94e9c21b688cc33dd7463922c7f340
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-06 22:05:01 -0500

    my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciIndex) {
+    const convertedFibonacciIndex = parseInt(fibonacciIndex)
+    if (fibonacciIndex < 0) return "OOPS"
+    if (fibonacciIndex === 0) return 0
+        
+    fibonacciArray = [1]
+    let precedingNumber1 = 0
+    let precedingNumber2 = 1
+        
+    for (let i=0; i<=fibonacciIndex-1; i++) {
+        let newNumber = precedingNumber1 + precedingNumber2
+        precedingNumber1 = precedingNumber2
+        precedingNumber2 = newNumber
+        fibonacciArray.push(newNumber)
+        }
+        return fibonacciArray[fibonacciIndex-1]
 
 };
 
