6acf666a8e9c10339a507eca75d0eb15de813c99
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-06 20:50:00 -0500

    my solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	const array = args[0]
+  let add = 0
+  for (let i=0; i<array.length; i++) {
+      add += array[i]
+  } 
+  return add
 };
 
-const multiply = function() {
+// official solution
+// const sum = function(array) {
+//   return array.reduce((total, current) => total + current, 0);
+// };
 
+const multiply = function(...args) {
+    const array = args[0]
+    let multiplication = 1
+    for (let i=0; i<array.length; i++) {
+      multiplication *= array[i]
+    }
+    return multiplication
 };
 
-const power = function() {
-	
+// Official solution
+// const multiply = function(array) {
+//   return array.length
+//     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+//     : 0;
+// };
+
+const power = function(base, exponent) {
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorialNumber = 1 
+  if (number === 0) {
+    return factorialNumber
+  } else {
+    for (i=number; i>0; i--) {
+    factorialNumber *= i
+   } 
+   return factorialNumber
+  }
 };
 
 // Do not edit below this line
