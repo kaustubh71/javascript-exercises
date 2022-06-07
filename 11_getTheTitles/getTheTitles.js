848996d429f179b9e4e55891706196beb904080e
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-07 12:25:04 -0500

    my solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titleArray = []
+    for (let i=0; i<books.length; i++) {
+        titleArray.push(books[i].title)
+    }
+    return titleArray
 };
+
+// Official solution
+// const getTheTitles = function(array) {
+//     return array.map(book => book.title)
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
