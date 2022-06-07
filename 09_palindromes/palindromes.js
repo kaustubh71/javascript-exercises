3b6541aa58853fbba5a329fabc8c58a04a772097
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-06 21:16:44 -0500

    I couldn't solve it and copied from the official solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+       const convertedString = string.lowercase().replace(/[^a-z]/g, "")
+       return ( 
+           convertedString.split("").reverse().join("") === convertedString
+       )
 };
 
 // Do not edit below this line
