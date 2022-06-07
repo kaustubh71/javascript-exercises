826ffdda1ac96c724f979e845797f56600af81d0
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-07 15:54:03 -0500

    couldn't solve. Understood from official solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+        const oldAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+        return oldAge > currentAge ? oldestPerson : currentPerson 
+    })
 
+    // Using the map function to get the age of each object in an array
+    // const age = people.map(({yearOfBirth, yearOfDeath}) => (yearOfDeath - yearOfBirth) )
+    // console.log(age)
+
+    //Using reduce function and ternary expression inside to get the oldest person from the age array
+    // console.log(age.reduce((previousValue, currentValue) => {
+    //     return (previousValue > currentValue) ? previousValue : currentValue
+    // } ))
 };
+
+const getAge = (birth, death) => {
+    if (!death) {
+        death = new Date().getFullYear()
+    }
+    return death - birth
+    }
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
