2b4a0c2f05051575e608ef208fa2599a2c00c8da
kaustubh71 <kaustubhkridutta@gmail.com>
2022-06-10 09:53:20 -0500

    couldn't solve. Understood from official solution

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,23 @@
-const caesar = function() {
-
+const caesar = function(string, shift) {
+    return string.split("").map((char) => charShift(char, shift)).join("")
 };
+
+const charShift = (char, shift) => {
+    const utfCode = char.charCodeAt()
+
+    if ((utfCode>=65 && utfCode<=90) || (utfCode>=97 && utfCode<=122)) {
+        return String.fromCharCode(
+            negativeNumberMod(utfCode + shift - codeSet(utfCode), 26) + codeSet(utfCode)
+        )
+    }
+    return char
+}
+
+const codeSet = code => code < 97 ? 65 : 97
+
+const negativeNumberMod = (n,m) => (n % m + m) % m
+
+
 
 // Do not edit below this line
 module.exports = caesar;
